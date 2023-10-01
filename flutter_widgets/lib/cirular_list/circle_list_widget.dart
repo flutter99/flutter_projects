18819aa9d5c5b43260b94a2caa828adb82409a42
Bilal Ahmad <82260050+flutter99@users.noreply.github.com>
2023-10-02 01:51:11 +0500

    Circular List Widget added

---
+++ flutter_widgets/lib/cirular_list/circle_list_widget.dart
@@ -0,0 +1,73 @@
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+
+
+class CircularList extends StatelessWidget {
+  final List<Widget> items;
+
+  const CircularList({
+    super.key,
+    required this.items,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.sizeOf(context).height;
+    return Stack(
+      children: List.generate(items.length, (index) {
+        final angle = 2 * math.pi * (index / items.length);
+        final radius = height * 0.16;
+        final x = radius * math.cos(angle);
+        final y = radius * math.sin(angle);
+        return Positioned(
+          left: x + 155, // center x
+          top: y + 155, // center y
+          child: items[index],
+        );
+      }),
+    );
+  }
+}
+
+class CircularListWidget extends StatelessWidget {
+  const CircularListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.sizeOf(context).height;
+    final double width = MediaQuery.sizeOf(context).width;
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        centerTitle: true,
+        title: Text('Circular List'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 600,
+              width: width,
+              child: CircularList(
+                items: List.generate(
+                  10,
+                  (index) => Container(
+                    height: 60,
+                    width: 60,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
